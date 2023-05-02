188535db8b2ff770c21cc98d929c748960af7435
jamesvrooney <jamesvrooney@hotmail.com>
2023-05-03 00:03:59 +0100

    Added customer12

--- src/main/java/com/jamesvrooney/controllers/GreetingController.java
+++ src/main/java/com/jamesvrooney/controllers/GreetingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
 
-    public void sayHello(String newCustomer11){
+    public void sayHello(String newCustomer12){
     }
 
 }
