086714c394e1e46764f78845c852324aa1c0a8a2
jamesvrooney <jamesvrooney@hotmail.com>
2023-05-02 22:58:11 +0100

    Initial commit.

---
+++ src/main/java/com/jamesvrooney/JenkinsTest3Application.java
@@ -0,0 +1,13 @@
+package com.jamesvrooney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTest3Application.class, args);
+	}
+
+}
